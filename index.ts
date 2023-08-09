ffb212e631a3f087e6e8260d0a0d475134a6bc04
Shamil <shamilvkage@gmail.com>
2023-08-09 12:47:52 +0530

    async added

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import sequelize from './models';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// db.sequelize.sync().then(() =>{
-//     app.listen(port, () => console.log(`App listening on ${port}`)
-//     );
-// })
+sequelize.authenticate().then(async () => {
+    await sequelize.sync()
+    app.listen(port, () => console.log(`App listening on ${port}`)
+    );
+})
